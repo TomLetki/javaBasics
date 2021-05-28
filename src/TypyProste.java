f67ff6576f76a4d837d2a23e36271010b3084a5c
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2021-05-28 10:26:55 +0200

    typy zmiennych

---
+++ src/TypyProste.java
@@ -0,0 +1,23 @@
+public class TypyProste {
+    //Typy proste
+
+    // liczby stałoprzecinkowe
+    byte firstNumber = 127; //-128 do 127
+    short secondNumber = 32689;
+    int thirdNumber = 32768999;
+    long = fourthNumber = -2^63 do (2^63)- 1
+
+    // liczby zmiennoprzecinkowe
+    float fifthNumber = 4,99999F; // 6 miejsc po przecniku
+    double sixthNumber = 3,9999999999D; // do 15 miejsc po rzecinku
+
+    // typy logiczne
+    boolean prawda = true;
+    boolean falsz = false;
+
+    // pojedynczy znak
+
+    char letter = 'a';
+
+
+}
