b8604104ce8ed473232ee590ad7f3eb1310b2ee2
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2021-05-28 09:53:54 +0200

    Powrót do wersji pierwotnej

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("World");
-        System.out.println("28.08.2021");
     }
 }
