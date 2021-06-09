92434a49e500bd447b4e1071371cc475d7a557ad
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2021-06-09 23:17:34 +0200

    nic

--- src/CustomScanner.java
+++ src/CustomScanner.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class CustomScanner {
    /* public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj imię: ");T
+        System.out.println("Podaj imię: ");
         String name = scanner.nextLine();
         System.out.println("Cześć "+ name + "!");
     }*/
