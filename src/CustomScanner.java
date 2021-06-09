ff389d5354a133ca4cb1a78e48e1c134fc14ac0b
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2021-06-09 17:51:28 +0200

    Dodanie out to gitignore

---
+++ src/CustomScanner.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class CustomScanner {
+   /* public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj imię: ");T
+        String name = scanner.nextLine();
+        System.out.println("Cześć "+ name + "!");
+    }*/
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadź liczbę");
+        int number = scanner.nextInt();
+        System.out.println(" Kwadrat liczby "+ number + " wynosi " + number*number);
+
+    }
+}
