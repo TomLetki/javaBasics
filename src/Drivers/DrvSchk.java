e5bf978fa5a84ff4ab29f90347ed91656d296870
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2021-07-14 09:49:14 +0200

    Klasa abstrakcyjne

--- src/Drivers/DrvSchk.java
+++ src/Drivers/DrvSchk.java
@@ -2,13 +2,22 @@ package Drivers;
 
 public class DrvSchk {
     public static void main(String[] args) {
-        ChromeDriver chrome = new ChromeDriver();
-        chrome.get();
-        chrome.findElementBy();
+        WebDriver driver = getDriver("firefox");
+        driver.get();
+        driver.findElementBy();
 
 
-        FirefoxDriver firefox = new FirefoxDriver();
-        firefox.get();
-        firefox.findElementBy();
+       // FirefoxDriver firefox = new FirefoxDriver();
+       // firefox.get();
+       // firefox.findElementBy();
             }
+
+    private static WebDriver getDriver(String name) {
+        if(name.equals("chrome")) {
+            return new ChromeDriver();
+        }else if(name.equals("firefox")){
+            return new FirefoxDriver();
+        }
+        return null;
+    }
 }
---
+++ src/animals/Cat.java
@@ -0,0 +1,10 @@
+package animals;
+
+public class Cat extends Animal{
+    @Override
+    public void sound() {
+        System.out.println("Koto robi miauu");
+        System.out.println("Kotek ma różek "+legs);
+
+    }
+}
